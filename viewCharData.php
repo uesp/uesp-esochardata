064f2fb56c5e49bea6706a906f791a6f5f2f386a
Dave Humphrey <uespnet@gmail.com>
2015-12-08 14:02:24 -0500

    tweak basic raw output

--- viewCharData.php
+++ viewCharData.php
@@ -1,3 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>UESP:ESO Character Data</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link rel="stylesheet" href="resources/esochardata.css" />
+		<script type="text/javascript" src="resources/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="resources/esochardata.js"></script>
+	</head>
+<body>
 <?php 
 
 
@@ -5,3 +15,11 @@ require_once("viewCharData.class.php");
 
 $charDataViewer = new EsoCharDataViewer();
 print($charDataViewer->getOutput());
+
+?>
+<hr />
+<div id='ecdFooter'>
+Created and hosted by the <a href="http://www.uesp.net">UESP</a>.
+</div>
+</body>
+</html>
