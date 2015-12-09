9d611ec10fe28a6e6ea37f2db6a27d365e33b0a5
Dave Humphrey <dave@uesp.net>
2015-12-09 18:45:33 -0500

    add cp points in skill tree

--- resources/esochardata.js
+++ resources/esochardata.js
@@ -105,9 +105,12 @@ function SelectEsoSkillTreeContents(object)
 	
 	EsoSkillTree_LastSkillContentName = object;
 	object.addClass('ecdSkillTreeNameHighlight2');
+	
+	var objText = object.contents().filter(function(){ return this.nodeType == 3;})[0].nodeValue;
+	
 	$('#ecdSkillContentTitle').text(object.text());
 	
-	var idName = 'ecdSkill_' + object.text().replace(" ", "").replace("'", "");
+	var idName = 'ecdSkill_' + objText.replace(" ", "").replace("'", "");
 	EsoSkillTree_LastSkillContent = $('#'+idName); 
 	EsoSkillTree_LastSkillContent.show(); 
 }
