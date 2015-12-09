9513a655007e74242191c2718dbf55603b1eedab
Dave Humphrey <uespnet@gmail.com>
2015-12-08 19:53:32 -0500

    add basic equip slots

--- viewCharData.php
+++ viewCharData.php
@@ -4,6 +4,8 @@
 		<title>UESP:ESO Character Data</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<link rel="stylesheet" href="resources/esochardata.css" />
+		<link rel="stylesheet" href="http://content3.uesp.net/esolog/resources/esoitemlink.css" />
+		<link rel="stylesheet" href="http://content3.uesp.net/esolog/resources/esoitemlink_embed.css" />
 		<script type="text/javascript" src="resources/jquery-1.10.2.js"></script>
 		<script type="text/javascript" src="resources/esochardata.js"></script>
 	</head>
