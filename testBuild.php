ae726d94a23b7838daffb0f0c8eb869b76b81e6e
Dave Humphrey <dave@uesp.net>
2016-06-15 15:47:07 -0400

    make test page load locally, tweak css

--- testBuild.php
+++ testBuild.php
@@ -10,24 +10,24 @@
 				font-size: 13px;
 			}
 		</style>
-		<link rel="stylesheet" href="http://esobuilds-static.uesp.net/resources/esobuilddata.css" />
-		<link rel="stylesheet" href="http://esolog-static.uesp.net/resources/esoitemlink.css" />
-		<link rel="stylesheet" href="http://esolog-static.uesp.net/resources/esoitemlink_embed.css" />
-		<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esoItemSearchPopup.css' />
+		<link rel="stylesheet" href="http://esobuilds.uesp.net/resources/esobuilddata.css" />
+		<link rel="stylesheet" href="http://esolog.uesp.net/resources/esoitemlink.css" />
+		<link rel="stylesheet" href="http://esolog.uesp.net/resources/esoitemlink_embed.css" />
+		<link rel='stylesheet' href='http://esolog.uesp.net/resources/esoItemSearchPopup.css' />
 		<link rel='stylesheet' href='http://www.uesp.net/w/extensions/UespEsoSkills/uespesoskills.css' />
-		<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esoskills_embed.css' />
-		<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esocp_simple_embed.css' />
+		<link rel='stylesheet' href='http://esolog.uesp.net/resources/esoskills_embed.css' />
+		<link rel='stylesheet' href='http://esolog.uesp.net/resources/esocp_simple_embed.css' />
 		<link rel='stylesheet' href='resources/esoEditBuild.css' />
 		<link rel='stylesheet' href='resources/esoEditBuild_embed.css' />
-		<script type="text/javascript" src="http://esobuilds-static.uesp.net/resources/jquery-1.10.2.js"></script>
-		<script type="text/javascript" src="http://esobuilds-static.uesp.net/resources/jquery.tablesorter.min.js"></script>
-		<script src='http://esobuilds-static.uesp.net/resources/json2.js'></script>
+		<script type="text/javascript" src="http://esobuilds.uesp.net/resources/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="http://esobuilds.uesp.net/resources/jquery.tablesorter.min.js"></script>
+		<script src='http://esobuilds.uesp.net/resources/json2.js'></script>
 		<script src='http://www.uesp.net/w/extensions/UespEsoItemLink/uespitemlink.js'></script>
-		<script src='http://esolog-static.uesp.net/resources/esoskills.js'></script>
-		<script src='http://esolog-static.uesp.net/resources/esocp_simple.js'></script>
-		<script type="text/javascript" src="http://esobuilds-static.uesp.net/resources/esobuilddata.js"></script>
+		<script src='http://esolog.uesp.net/resources/esoskills.js'></script>
+		<script src='http://esolog.uesp.net/resources/esocp_simple.js'></script>
+		<script type="text/javascript" src="http://esobuilds.uesp.net/resources/esobuilddata.js"></script>
 		<script src='http://www.uesp.net/w/extensions/UespEsoSkills/uespesoskills.js?4Apr2016'></script>
-		<script src='http://esolog-static.uesp.net/resources/esoItemSearchPopup.js'></script>
+		<script src='http://esolog.uesp.net/resources/esoItemSearchPopup.js'></script>
 		<script src='resources/esoEditBuild.js'></script>
 		
 	</head>
