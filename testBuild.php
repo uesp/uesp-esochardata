9599aababc853e2d3de2ff393f729b9e3595959a
Dave Humphrey <dave@uesp.net>
2016-06-15 15:05:38 -0400

    add standalone version of build editor for easier testing

---
+++ testBuild.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>UESP:ESO Character Build Editor</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<style type="text/css">>
+			body {
+				background-color: #FBEFD5;
+				font-family: sans-serif;
+				font-size: 13px;
+			}
+		</style>
+		<link rel="stylesheet" href="http://esobuilds-static.uesp.net/resources/esobuilddata.css" />
+		<link rel="stylesheet" href="http://esolog-static.uesp.net/resources/esoitemlink.css" />
+		<link rel="stylesheet" href="http://esolog-static.uesp.net/resources/esoitemlink_embed.css" />
+		<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esoItemSearchPopup.css' />
+		<link rel='stylesheet' href='http://www.uesp.net/w/extensions/UespEsoSkills/uespesoskills.css' />
+		<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esoskills_embed.css' />
+		<link rel='stylesheet' href='http://esolog-static.uesp.net/resources/esocp_simple_embed.css' />
+		<link rel='stylesheet' href='resources/esoEditBuild.css' />
+		<link rel='stylesheet' href='resources/esoEditBuild_embed.css' />
+		<script type="text/javascript" src="http://esobuilds-static.uesp.net/resources/jquery-1.10.2.js"></script>
+		<script type="text/javascript" src="http://esobuilds-static.uesp.net/resources/jquery.tablesorter.min.js"></script>
+		<script src='http://esobuilds-static.uesp.net/resources/json2.js'></script>
+		<script src='http://www.uesp.net/w/extensions/UespEsoItemLink/uespitemlink.js'></script>
+		<script src='http://esolog-static.uesp.net/resources/esoskills.js'></script>
+		<script src='http://esolog-static.uesp.net/resources/esocp_simple.js'></script>
+		<script type="text/javascript" src="http://esobuilds-static.uesp.net/resources/esobuilddata.js"></script>
+		<script src='http://www.uesp.net/w/extensions/UespEsoSkills/uespesoskills.js?4Apr2016'></script>
+		<script src='http://esolog-static.uesp.net/resources/esoItemSearchPopup.js'></script>
+		<script src='resources/esoEditBuild.js'></script>
+		
+	</head>
+<body>
+<?php
+
+require_once("editBuild.class.php");
+
+
+$buildDataEditor = new EsoBuildDataEditor();
+print($buildDataEditor->GetOutputHtml());
+
+?>
+<hr />
+<div id='ecdFooter'>
+Created and hosted by the <a href="http://www.uesp.net">UESP</a>.
+</div>
+</body>
+</html>
