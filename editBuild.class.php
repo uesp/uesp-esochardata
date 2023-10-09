a57e5ac0748909d55e75cf56851a342d60f7e083
Dave Humphrey <dave@uesp.net>
2023-10-09 10:06:57 -0400

    update 40pts

--- editBuild.class.php
+++ editBuild.class.php
@@ -33,7 +33,7 @@ class EsoBuildDataEditor
 	
 	public $LOAD_RULES_FROM_DB = true;
 	public $LIVE_RULES_VERSION = "39";
-	public $PTS_RULES_VERSION  = "39pts";
+	public $PTS_RULES_VERSION  = "40pts";
 	
 	public $SESSION_DEBUG_FILENAME = "/var/log/httpd/esoeditbuild_sessions.log";
 	
