c26025b172133b2cead3e332d22376850789886a
Dave Humphrey <dave@uesp.net>
2025-06-02 16:22:32 -0400

    update 46

--- editBuild.class.php
+++ editBuild.class.php
@@ -32,7 +32,7 @@ class EsoBuildDataEditor
 	public $PTS_VERSION = "46pts";	//TODO: Remove?
 	
 	public $LOAD_RULES_FROM_DB = true;
-	public $LIVE_RULES_VERSION = "45";
+	public $LIVE_RULES_VERSION = "46";
 	public $PTS_RULES_VERSION  = "46pts";
 	
 	public $READONLY = false;
